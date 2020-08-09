965cadf903e88ce4bd4e12291ce1c588b9604cd1
Leo Souza <leofelipe222@users.noreply.github.com>
2020-08-09 12:08:48 -0700

    Setting Debug to False

--- soskardec/settings.py
+++ soskardec/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0k27d#=v3iw&o8(=x3iz$!9$wp%b4y@3hh88=x78k+4%70xm_&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['soskardec.herokuapp.com','10.0.0.23']
 
